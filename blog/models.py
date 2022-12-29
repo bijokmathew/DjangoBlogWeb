8aff507bd83715a8e8ad04d9feea57dffaae548b
bijokmathew <bijo.nbr@hotmail.com>
2022-12-29 19:20:24 +0000

    added Proc file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='place holder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
+    
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return str(self.title)
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(unique=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='comment')
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comments {self.body} by {self.name}"
+
